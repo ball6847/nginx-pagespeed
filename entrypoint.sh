fa9338365ba7cab1f27738e3c87c14f8d6f8bb1b
Porawit Poboonma <porawit.p@bizidea.co.th>
2016-04-20 15:06:59 +0700

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ set -e
 
 NGINX_USER=www-data
 NGINX_GROUP=www-data
+NGINX_LOG_PATH=/var/log/nginx
 NGINX_CACHE_PATH=/var/cache/nginx
 PAGESPEED_CACHE_PATH=/var/cache/ngx_pagespeed
 
-mkdir -p $NGINX_CACHE_PATH
-mkdir -p $PAGESPEED_CACHE_PATH
+mkdir -p $NGINX_LOG_PATH $NGINX_CACHE_PATH $PAGESPEED_CACHE_PATH
 chown -R "$NGINX_USER:$NGINX_GROUP" $NGINX_CACHE_PATH $PAGESPEED_CACHE_PATH
 
 nginx -g "daemon off;"
